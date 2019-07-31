2288bb35a0c29458628f1892cb4c1cb3b7c45300
Mayukh Sinha <33156064+mayukhsinha11699@users.noreply.github.com>
2019-07-31 10:11:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomdiceimage = "dice" + randomNumber1 + ".png";
 
-var source = "images/" + randomdiceimage;
-
 
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", source);
+image1.setAttribute("src", randomdiceimage);
